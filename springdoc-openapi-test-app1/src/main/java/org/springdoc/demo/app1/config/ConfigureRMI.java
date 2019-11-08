586c47549b4a7791650ede7ff7a949b2b0138450
Thomas Pasch <thomas.pasch@gmx.de>
2019-11-08 17:43:54 +0100

    more spring/jmx

--- springdoc-openapi-test-app1/src/main/java/org/springdoc/demo/app1/config/ConfigureRMI.java
+++ springdoc-openapi-test-app1/src/main/java/org/springdoc/demo/app1/config/ConfigureRMI.java
@@ -2,12 +2,19 @@ package org.springdoc.demo.app1.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
+import org.springframework.jmx.access.MBeanProxyFactoryBean;
 import org.springframework.jmx.support.ConnectorServerFactoryBean;
+import org.springframework.jmx.support.MBeanServerConnectionFactoryBean;
 import org.springframework.remoting.rmi.RmiRegistryFactoryBean;
+
+import javax.management.MBeanServerConnection;
+import javax.management.MalformedObjectNameException;
+import java.net.MalformedURLException;
 
 /**
  * https://stackoverflow.com/questions/29412072/how-to-access-spring-boot-jmx-remotely
@@ -23,6 +30,16 @@ public class ConfigureRMI {
     @Value("${jmx.rmi.port:1099}")
     private Integer rmiPort;
 
+    private String getObjectName() {
+        return "connector:name=rmi";
+    }
+
+    private String getServiceUrl() {
+        String url = String.format("service:jmx:rmi://%s:%s/jndi/rmi://%s:%s/jmxrmi", rmiHost, rmiPort, rmiHost, rmiPort);
+        // String url = "service:jmx:jmxmp://" + rmiHost + ":" + rmiPort;
+        return url;
+    }
+
     @Bean
     public RmiRegistryFactoryBean rmiRegistry() {
         final RmiRegistryFactoryBean rmiRegistryFactoryBean = new RmiRegistryFactoryBean();
@@ -32,14 +49,41 @@ public class ConfigureRMI {
         return rmiRegistryFactoryBean;
     }
 
+    @Qualifier("serverMBeanConnection")
     @Bean
     @DependsOn("rmiRegistry")
-    public ConnectorServerFactoryBean connectorServerFactoryBean() throws Exception {
+    public ConnectorServerFactoryBean connectorServerFactoryBean()
+            throws MalformedObjectNameException
+    {
         final ConnectorServerFactoryBean connectorServerFactoryBean = new ConnectorServerFactoryBean();
-        connectorServerFactoryBean.setObjectName("connector:name=rmi");
-        String url = String.format("service:jmx:rmi://%s:%s/jndi/rmi://%s:%s/jmxrmi", rmiHost, rmiPort, rmiHost, rmiPort);
-        LOG.info("service url: " + url);
+        connectorServerFactoryBean.setObjectName(getObjectName());
+        String url = getServiceUrl();
+        LOG.info("service url server: " + url);
         connectorServerFactoryBean.setServiceUrl(url);
         return connectorServerFactoryBean;
+    }
+
+    @Qualifier("clientMBeanConnection")
+    @Bean
+    public MBeanServerConnectionFactoryBean mBeanServerConnectionFactoryBean()
+            throws MalformedURLException
+    {
+        MBeanServerConnectionFactoryBean result = new MBeanServerConnectionFactoryBean();
+        String url = getServiceUrl();
+        LOG.info("service url client: " + url);
+        result.setServiceUrl(url);
+        return result;
+    }
+
+    @Bean
+    public MBeanProxyFactoryBean mBeanProxyFactoryBean(
+            @Qualifier("clientMBeanConnection") MBeanServerConnection connection)
+            throws MalformedURLException, MalformedObjectNameException
+    {
+        MBeanProxyFactoryBean result = new MBeanProxyFactoryBean();
+        result.setObjectName(getObjectName());
+        result.setServiceUrl(getServiceUrl());
+        result.setServer(connection);
+        return result;
     }
 }
